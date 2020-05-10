c8188903402a96ade585fc571f816b259f3296d6
dunicode <dunicode@TUX.localdomain>
2020-05-10 01:35:19 -0400

    Playing with tokens

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -11,4 +11,35 @@ class Page extends BaseController
 	{
 		return $this->respond(['data' => 'Main Page JSON'], 200);
 	}
+
+	public function test()
+	{
+		//STORE USER TOKEN TO VERIFY FROM HERE AND INVALIDATE TOKEN
+		$token[] = 'token1';
+		cache()->save('auth_token', $token, 2592000);
+	}
+
+	public function test2()
+	{
+		$new = 'token2';
+		$cache = \Config\Services::cache();
+		$almacen = $cache->get('auth_token');
+		$almacen[] = $new;
+		cache()->save('auth_token', $almacen, 2592000);
+		print_r($cache->get('auth_token'));
+		//$cache->delete('foo');
+	}
+
+	public function test3()
+	{
+		$cache = \Config\Services::cache();
+		$almacen = $cache->get('auth_token');
+
+		if (array_search('token2', $almacen)) {
+			print_r("Si esta");
+		}else {
+			print_r("No esta");
+		}
+
+	}
 }
